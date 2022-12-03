2acdcf8f4f4389ec54f4e31d81dbd43bb3e153a1
Anastasia Evmenova <nastya-evmenova@mail.ru>
2022-12-04 00:34:04 +0300

    Добавьте файлы проекта.

---
+++ Parent/Parent.cpp
@@ -0,0 +1,66 @@
+﻿#include<iostream> 
+#include<windows.h> 
+#include<conio.h> 
+#include<stdio.h> 
+#include<tchar.h> 
+#include<string> 
+
+using namespace std;
+
+
+
+int main()
+{
+	int size;
+	cout << "Enter size of array: ";
+	cin >> size;
+
+	int* array = new int[size];
+	cout << "Enter " << size << " elements: " << endl;
+	for (int i = 0; i < size; i++)
+	{
+		cin >> array[i];
+	}
+
+	unsigned int Y;
+	cout << "Enter Y shift: ";
+	cin >> Y;
+
+	string cmd_args;
+	cmd_args = to_string(Y);
+	cmd_args += " ";
+	cmd_args += to_string(size);
+	cmd_args += " ";
+	for (int i = 0; i < size; i++)
+	{
+		cmd_args += to_string(array[i]);
+		if (i != size - 1)
+		{
+			cmd_args += " ";
+		}
+	}
+
+	wstring stemp = wstring(cmd_args.begin(), cmd_args.end());
+	LPCTSTR lpszAppName = L"child.exe";
+	STARTUPINFO si;
+	PROCESS_INFORMATION piApp;
+	ZeroMemory(&si, sizeof(STARTUPINFO));
+	si.cb = sizeof(STARTUPINFO);
+	si.dwFlags = STARTF_USEPOSITION;
+	si.dwY = Y;
+
+	if (!CreateProcess(lpszAppName, &stemp[0], NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &piApp))
+	{
+		cout << "No new process" << endl;
+		return 0;
+	}
+
+	cout << "New process is created" << endl;
+	WaitForSingleObject(piApp.hProcess, INFINITE);
+
+	CloseHandle(piApp.hThread);
+	CloseHandle(piApp.hProcess);
+	delete[]array;
+	return 0;
+
+}
---
+++ child/child.cpp
@@ -0,0 +1,46 @@
+﻿#include<iostream> 
+#include<stdlib.h> 
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+
+	cout << "Child created" << endl;
+	int Y = atoi(argv[0]);
+	int size = atoi(argv[1]);
+	cout << "Number to find is " << Y << endl;
+	cout << "Size of array is " << size << endl;
+
+	int* mas = new int[size];
+	int counter = 0;
+	for (int i = 2; i < argc; i++)
+	{
+		mas[counter] = atoi(argv[i]);
+		counter += 1;
+	}
+
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size-1; j++) {
+			if (mas[j] > mas[j + 1]) {
+				int b = mas[j]; 
+				mas[j] = mas[j + 1]; 
+				mas[j + 1] = b; 
+			}
+		}
+	}
+
+
+	for (int i = 0; i < size; i++) {
+		cout << mas[i] << " ";
+	}
+
+
+	cout << endl;
+
+	system("pause");
+	delete[] mas;
+	return 0;
+
+}
